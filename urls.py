99f3d25380bc7e2d33f58796da0b391325c16c65
Mikayn <ghimirearyan21@gmail.com>
2025-08-08 13:12:01 +0000

    First Commit

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("create/", views.CreateAuction, name="create"),
+    path("wishlist/", views.wishlist, name="wishlist"),
+    path("categories/", views.categories, name="categories"),
+    path("listing/<int:id>/", views.listing, name="listing")
+]
